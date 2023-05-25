8a3049a7a5551ab9493f4ee0b0a697904930b2cb
Leonardo Burbano <leofr7nco@hotmail.com>
2023-05-25 11:54:03 -0500

    Updated

---
+++ 8_login/src/public/js/register.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('registerForm');
+
+
+form.addEventListener('submit', e=>{
+    console.log("Submit register")
+    e.preventDefault();
+    const data = new FormData(form);
+    const obj = {};
+    data.forEach((value,key)=>obj[key]=value);
+    fetch('/api/session/register',{
+        method:'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(result=>{
+        if(result.status == 200){
+            window.location.replace('/')
+        }
+    })
+})
